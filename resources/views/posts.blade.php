19f2acb9e74155352ccffe7dd2934428a5d76cdd
SHIMANTO50 <shimantotsp@gmail.com>
2023-07-02 15:42:16 +0600

    Assignment Updated

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -7,14 +7,18 @@
     <title>Document</title>
 </head>
 <body>
-    <table>
+    <table border="1">
         <tr>
             <th>Name</th>
+            <th>Title</th>
+            <th>Description</th>
         </tr>
 
         @foreach ($posts as $post)
             <tr>
                 <td>{{$post->category->name}}</td>
+                <td>{{$post->title}}</td>
+                <td>{{$post->desc}}</td>
             </tr>
           
 
